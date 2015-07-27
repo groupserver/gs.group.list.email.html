07d2f1fd7fd82b2b90cf324a90d1d3a1a38164e2
Michael JasonSmith <mpj17@onlinegroups.net>
2015-07-27 14:48:32 -0700

    Adding the HTMLMessage.as_email method

--- gs/group/list/email/html/message.py
+++ gs/group/list/email/html/message.py
@@ -13,6 +13,7 @@
 #
 ############################################################################
 from __future__ import absolute_import, unicode_literals, print_function
+from email.mime.text import MIMEText
 from zope.cachedescriptors.property import Lazy
 from zope.component import createObject
 from gs.content.email.base import (GroupEmail)
@@ -25,8 +26,8 @@ class HTMLMessage(GroupEmail):
 :type post: gs.group.list.email.base.interfaces.IPost
 :param request: The Zope request
 
-Mostly this class exists just to set the correct headers. The heavy-lifting
-is done by the viewlets.'''
+Mostly this class exists just to set the correct headers. The
+heavy-lifting is done by the viewlets.'''
     def __init__(self, post, request):
         super(HTMLMessage, self).__init__(post, request)
 
@@ -40,4 +41,15 @@ is done by the viewlets.'''
         'The person who authored the post'
         retval = createObject('groupserver.UserFromId',
                               self.groupInfo.groupObj, self.post['user_id'])
+        return retval
+
+    def as_email(self):
+        '''The HTML message as an email-component
+
+:returns: An instance of the core email-message class containing the HTML
+          form of the post. The MIME-type of the message is
+          :mimetype:`text/html`, and the encoding is UTF-8.
+:rtype: :class:`email.mime.text.MIMEText`'''
+        html = self()
+        retval = MIMEText(html, 'html', 'utf-8')
         return retval
