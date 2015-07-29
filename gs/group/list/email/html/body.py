0323f63cbe3e7ea8429d7a42c590998e6f952960
Michael JasonSmith <mpj17@onlinegroups.net>
2015-07-28 19:59:26 -0700

    Markup words

--- gs/group/list/email/html/body.py
+++ gs/group/list/email/html/body.py
@@ -13,6 +13,7 @@
 #
 ############################################################################
 from __future__ import absolute_import, unicode_literals
+from re import compile as re_compile, I as re_I, M as re_M, U as re_U
 from xml.sax.saxutils import escape
 from zope.cachedescriptors.property import Lazy
 from gs.group.list.email.base import EmailMessageViewlet
@@ -33,8 +34,39 @@ class BodyViewlet(EmailMessageViewlet):
         retval['url'] = '{0}/r/post/{1}'.format(self.siteInfo.url, retval['post_id'])
         return retval
 
-    @classmethod
-    def markup(cls, line):
+    bold_matcher = re_compile("(\*.*\*)")
+    email_matcher = re_compile(r"(.*?)([A-Z0-9\._%+-]+@[A-Z0-9.-]+\.[A-Z]+)(.*?)",
+                               re_I | re_M | re_U)
+    www_matcher = re_compile("(?i)(www\..+)")
+    uri_matcher = re_compile("(?i)(http://|https://)(.+?)(\&lt;|\&gt;"
+                             "|\)|\]|\}|\"|\'|$|\s)")
+
+    def markup_words(self, line):
+        # --=mpj17=-- I am not proud
+        words = line.split(' ')
+        rwords = []
+        for word in words:
+            if self.bold_matcher.match(word):
+                w = self.bold_matcher.sub(r'<b>\g<1></b>', word)
+            elif self.email_matcher.match(word):
+                print('"{}"'.format(word))
+                w = self.email_matcher.sub(
+                    '<a class="email" href="mailto:\g<2>">\g<1>\g<2>\g<3></a>',
+                    word)
+                print('"{}"'.format(w))
+            elif self.www_matcher.match(word):
+                w = self.www_matcher.sub('<a href="http://\g<1>">\g<1></a>',
+                                         word)
+            elif self.uri_matcher.match(word):
+                w = self.uri_matcher.sub(
+                    '<a href="\g<1>\g<2>">\g<1>\g<2></a>\g<3>', word)
+            else:
+                w = word
+            rwords.append(w)
+        retval = ' '.join(rwords)
+        return retval
+
+    def markup(self, line):
         if line.strip() == '':
             retval = '&#160;<br/>'
         else:
@@ -42,9 +74,10 @@ class BodyViewlet(EmailMessageViewlet):
             if line.lstrip()[0] == '>':
                 cssClass += " muted"
             #  <https://wiki.python.org/moin/EscapingHtml>
-            escapedLine = escape(line.rstrip(), cls.HTML_ESCAPE_TABLE)
+            escapedLine = escape(line.rstrip(), self.HTML_ESCAPE_TABLE)
+            markedUpLine = self.markup_words(escapedLine)
             r = '<span class="{0}">{1}</span><br/>'
-            retval = r.format(cssClass, escapedLine)
+            retval = r.format(cssClass, markedUpLine)
         assert(retval)
         return retval
 
